96168bf392a69dab5975c64f8ec31d06fda8d54c
Wasim <wasimxaman13@gmail.com>
2024-03-15 12:03:03 +0500

    Refactor Player class and add Stats class

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,40 +1,69 @@
 class Player {
-  String? sId;
+  String? id;
   String? name;
   String? location;
   String? role;
-  int? age;
+  String? age;
   String? additionalInfo;
   List<String>? admins;
+  Stats? stats;
 
-  Player(
-      {this.sId,
-      this.name,
-      this.location,
-      this.role,
-      this.age,
-      this.additionalInfo,
-      this.admins});
+  Player({
+    this.name,
+    this.id,
+    this.location,
+    this.role,
+    this.age,
+    this.additionalInfo,
+    this.admins,
+    this.stats,
+  });
 
   Player.fromJson(Map<String, dynamic> json) {
-    sId = json['_id'];
+    id = json['_id'];
     name = json['name'];
     location = json['location'];
     role = json['role'];
     age = json['age'];
     additionalInfo = json['additionalInfo'];
-    admins = json['admin'].cast<String>();
+    admins = json['admins'].cast<String>();
+    stats = json['stats'] != null ? Stats.fromJson(json['stats']) : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['_id'] = sId;
+    data['_id'] = id;
     data['name'] = name;
     data['location'] = location;
     data['role'] = role;
     data['age'] = age;
     data['additionalInfo'] = additionalInfo;
-    data['admin'] = admins;
+    data['admins'] = admins;
+    if (stats != null) {
+      data['stats'] = stats!.toJson();
+    }
+    return data;
+  }
+}
+
+class Stats {
+  String? sixes;
+  String? fours;
+  String? wickets;
+
+  Stats({this.sixes, this.fours, this.wickets});
+
+  Stats.fromJson(Map<String, dynamic> json) {
+    sixes = json['sixes'];
+    fours = json['fours'];
+    wickets = json['wickets'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['sixes'] = sixes;
+    data['fours'] = fours;
+    data['wickets'] = wickets;
     return data;
   }
 }
